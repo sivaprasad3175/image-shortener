1c2d8ed3f7bf6ebd8b0c1f8fd93e127c8a0eedba
siva <sivap3038@gmail.com>
2026-03-14 15:27:10 +0530

    'added'

--- server/app.js
+++ server/app.js
@@ -10,6 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ===== Get URL Path Logic =====
+app.use((req, res, next) => {
+  const protocol = req.protocol;
+  const host = req.get('host');
+  const path = req.originalUrl;
+
+  const fullUrl = `${protocol}://${host}${path}`;
+
+  console.log("Method:", req.method);
+  console.log("Path:", path);
+  console.log("Full URL:", fullUrl);
+
+  next();
+});
+
 // Routes
 app.use('/', imageRoutes);
 
